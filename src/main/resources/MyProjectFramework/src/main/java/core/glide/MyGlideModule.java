4f1daf428ab5dceabe332b6eadcff57f1cc57677
dusunboy <du_sunboy@qq.com>
2017-09-12 00:29:21 +0800

    20170912

--- src/main/resources/MyProjectFramework/src/main/java/core/glide/MyGlideModule.java
+++ src/main/resources/MyProjectFramework/src/main/java/core/glide/MyGlideModule.java
@@ -4,20 +4,24 @@ import android.content.Context;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
-import com.bumptech.glide.module.GlideModule;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
 
 /**
  * 我的GlideModule
  * Created by Vincent on $Time.
  */
-public class MyGlideModule implements GlideModule {
+@GlideModule
+public class MyGlideModule implements AppGlideModule {
 
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
     }
 
     @Override
-    public void registerComponents(Context context, Glide glide) {
+    public void registerComponents(Context context, Glide glide, Registry registry) {
 
     }
 }
